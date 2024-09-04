5be2ceef363f9367e18c7374d415a90d1234daec
Alejandro Ku León <alexku05100@gmail.com>
2024-09-04 14:02:22 -0600

    Reto1.py

--- Reto1.py
+++ Reto1.py
@@ -7,17 +7,17 @@ num2 = float(input("Ingrese un segundo numero: "))
 if operacion == '+':
     resultado = num1 + num2
     print("Resultado: ",resultado)
-if operacion == '-':
+elif operacion == '-':
     resultado = num1 - num2
     print("Resultado: ",resultado)
-if operacion == '*':
+elif operacion == '*':
     resultado = num1 * num2
     print("Resultado: ",resultado)
-if operacion == '/':
+elif operacion == '/':
     if num2 == 0:
             print("No existe divison entre cero")
     else:
             resultado = num1 / num2
-            print(" Resultado: ",resultado)
+            print(" Resultado: ",resultado)            
 else:
-    print("Operador invalido")
+ print("Operador invalido")
